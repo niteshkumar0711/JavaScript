504f07aa5668dc1fb75163173c9319fa33edd7c7
Nitesh Kumar <niteshkr9155166021@gmail.com>
2024-08-09 16:25:10 +0000

    datatypes

---
+++ new/02_variables.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3)  //we are using nodejs, not browser so it won't run here
+
+console.log(3 
+    + 3);  //code readability is required.
+
+console.log("Nitesh")
+
+let name = "Nitesh"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object 
+
+console.log(typeof undefined);  //it is undefined
+console.log(typeof null);  //null is object
+
