b21f331ffc8c839b90a0bc581301c847a981a6ac
Nitesh Kumar <niteshkr9155166021@gmail.com>
2024-08-09 17:13:07 +0000

    Operations

--- new/03_conversionOperation.js
+++ new/03_conversionOperation.js
@@ -27,10 +27,65 @@ true => 1; false => 0
 "nitesh" => true
 */
 
-let someNumber = 33
+// let someNumber = 33
  
-let stringNumber = String(someNumber)
-console.log(stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
 
+
+// *********************** OPERATIONS ******************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Nitesh"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+let one = "1"
+let two = 2
+let ans = one + two
+
+// console.log(ans);
+// console.log(typeof one, typeof two, typeof ans);  // string number string
+
+
+
+// let one = 1
+// let two = 2
+// let three = "3"
+// let ans = one + two + three
+
+// console.log(ans);
+// console.log(typeof one, typeof two,typeof three, typeof ans);  // number number string string
+
+// console.log(+true);  // output is 1
+// console.log(true);  // error
+// console.log(+"");  // output is 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
